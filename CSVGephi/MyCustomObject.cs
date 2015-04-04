57a303e6902512808b54977e2e728cc065be1dbe
Infuscy <buse.florinx@gmail.com>
2015-04-04 10:43:57 +0300

    Creating initial version.

---
+++ CSVGephi/MyCustomObject.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSVGephi
+{
+    class MyCustomObject
+    {
+        public String Deprel { get; set; }
+
+        public String Head { get; set; }
+
+        public String Id { get; set; }
+
+        public String Lemma { get; set; }
+
+        public String Pos { get; set; }
+
+        public String WText { get; set; }
+
+        public int SId { get; set; }
+    }
+}
---
+++ UserParser/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace UserParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlDocument userDoc = new XmlDocument();
+            userDoc.Load(@"C:\Users\Florin\Desktop\xml-uril comentarii&useri&regiuni\UserFinal.xml");
+
+            XmlNodeList xmlNodeList = userDoc.SelectNodes("//users/user");
+
+            Dictionary<string, int> userCategories = new Dictionary<string, int>();
+
+            foreach(XmlNode xmlNode in xmlNodeList)
+            {
+                string category = xmlNode.Attributes["user_type"].Value;
+                if(userCategories.ContainsKey(category))
+                {
+                    int numberOfUsers = userCategories[category];
+
+                    userCategories[category] = numberOfUsers + 1;
+                }
+                else
+                {
+                    userCategories.Add(category, 1);
+                }
+                    
+            }
+
+            string temp = null;
+            foreach(var item in userCategories.ToList())
+            {
+                temp += item.Key + " " + item.Value + "\n";
+            }
+            File.AppendAllText(@"C:\Users\Florin\Desktop\xml-uril comentarii&useri&regiuni\Users.csv", temp);
+        }
+    }
+}
